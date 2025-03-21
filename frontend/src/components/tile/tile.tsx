461f7f64c06dc51697fb97831279dca364b1ace8
birgilv <birgittevik84@gmail.com>
2025-03-21 11:35:13 +0100

    Added more discriptive comments

--- frontend/src/components/tile/tile.tsx
+++ frontend/src/components/tile/tile.tsx
@@ -3,26 +3,30 @@ import "./Tile.css";
 /**
  * Tile Component
  *
- * This component represents an individual tile on a chessboard. The tile color is 
- * determined based on whether the given number is even or odd. An optional image 
- * (such as a chess piece) can be displayed on the tile.
+ * This component represents an individual square on a chessboard.
+ * It dynamically determines the tile color (black or white) based on the provided `number`,
+ * which encodes both the row and column indices. If an `image` prop is passed, it displays
+ * a chess piece on the tile using a background image.
  */
 
 interface Props {
-  number: number;
-  image?: string;
+  number: number; // Used to determine tile color
+  image?: string; // Optional image URL for a chess piece
 }
 
 function Tile({ number, image }: Props) {
+   // Determine the tile color: even = black, odd = white
   if (number % 2 === 0) {
     return (
       <div className="tile black-tile">
+        {/* If an image URL is provided, render the piece using a div with background image */}
         {image && <div style={{ backgroundImage: `url(${image})` }} className="chess-piece"></div>}
       </div>
     );
   } else {
     return (
       <div className="tile white-tile">
+        {/* If an image URL is provided, render the piece using a div with background image */}
         {image && <div style={{ backgroundImage: `url(${image})` }} className="chess-piece"></div>}
       </div>
     );
