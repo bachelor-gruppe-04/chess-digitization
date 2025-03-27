6ca4ff333ccddb134860c52344ae5bedb2629946
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2025-03-27 14:57:33 +0100

    Created Board class to handle move_history, camera allocations, and game simulation

--- backend/logic/api/board.py
+++ backend/logic/api/board.py
@@ -1,14 +1,58 @@
+import chess
+
 from camera import Camera
 from typing import List
 
 class Board:
-    def __init__(self, id: int):
-        self.id = id
-        self.camera = Camera(id)
-        self.move_history: List[str] = []
-
-    def get_camera(self) -> Camera:
-        return self.camera
-
-    def get_move_history(self) -> List[str]:
-        return self.move_history
+  
+  def __init__(self, id: int):
+    self.set_id(id)
+    self.camera: Camera = Camera(id)
+    self.move_history: List[str] = []
+    self.chess_board: chess.Board = chess.Board()
+  
+  def set_id(self, id: int) -> TypeError | None:
+    if not int.is_integer(id):
+      raise TypeError
+      
+    self.id = id
+    
+  def get_id(self) -> int:
+    return self.id
+    
+  def get_camera(self) -> Camera:
+    return self.camera
+  
+  def get_move_history(self) -> List[str]:
+    return self.move_history
+  
+  def get_chess_board(self) -> chess.Board:
+    return self.chess_board
+  
+  def move_piece(self, move: str) -> str:
+    is_valid: bool = self.validate_move(move)
+    command: str = ""
+    
+    if is_valid:
+      self.move_history.append(move)
+      command = move, is_valid
+      
+    else: 
+      command = "INVALID", is_valid
+      
+    return command
+      
+  def validate_move(self, move: str) -> bool:
+    is_valid: bool = None
+    
+    try:
+      self.chess_board.push_san(move)
+      is_valid = True
+    except Exception: # chess.InvalidMoveError | chess.IllegalMoveError | chess.AmbiguousMoveError:
+      is_valid = False
+    
+    return is_valid
+  
+  def reset_board(self) -> str:
+    self.chess_board.reset()
+    return "RESET"
