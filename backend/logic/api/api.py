3c6c50fedcd7259000fa44b9c74886a133f369ff
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2025-03-28 11:56:50 +0100

    WebSocket now communcated based on board_id

--- backend/logic/api/api.py
+++ backend/logic/api/api.py
@@ -1,5 +1,4 @@
 import asyncio
-
 from fastapi import FastAPI, WebSocket, Path
 from fastapi.responses import StreamingResponse
 from typing import List, Dict
@@ -9,8 +8,7 @@ from camera import Camera
 app = FastAPI()
 
 clients = []
-move_history: List[str] = []
-boards: Dict[int, Board] = {i: Board(i) for i in range(1)}
+boards: Dict[int, Board] = {i: Board(i) for i in range(3)}
 
 @app.get("/video/{id}")
 def video_feed(id: int = Path(..., ge=0, le=(len(boards) - 1))) -> StreamingResponse:
@@ -22,57 +20,62 @@ def video_feed(id: int = Path(..., ge=0, le=(len(boards) - 1))) -> StreamingResp
     )
   return {"error": "Invalid camera ID"}
 
-@app.websocket("/moves/0")
-async def websocket_endpoint(websocket: WebSocket) -> None:
+@app.websocket("/moves/{board_id}")
+async def websocket_endpoint(websocket: WebSocket, board_id: int) -> None:
   """ Sends chess moves and history.
   
   Args:
     websocket (WebSocket): WebSocket connection
   """
   await websocket.accept()
-  clients.append(websocket)
+  if board_id not in boards:
+    await websocket.close()
+    return
+    
+  boards[board_id].clients.append(websocket)
   try:
-    for move in move_history:
+    for move in boards[board_id].move_history:
       await websocket.send_text(move)
     while True:
       await websocket.receive_text()
   except:
-    clients.remove(websocket)
+    boards[board_id].clients.remove(websocket)
 
-async def send_move(move: str) -> None:
+async def send_move(board_id: int, move: str) -> None:
   """ Send a chess move to all clients.
 
   Args:
     move (str): Chess move
   """
-  move_history.append(move)
-  for client in clients:
+  boards[board_id].move_piece(move)
+  for client in boards[board_id].clients:
     await client.send_text(move)
 
-async def reset_game() -> None:
+async def reset_game(board_id: int) -> None:
   """ Reset the chess game. """
-  global move_history
-  move_history = []
-  for client in clients:
+  boards[board_id].move_history = []
+  for client in boards[board_id].clients:
     await client.send_text("RESET")
     
 async def fake_ml_moves() -> None:
   """ Simulate a chess game using hardcoded moves. """
-  moves = ["e4", "e5", "Nf3", "Nc6", "Bb5", "a6"]
-  for move in moves:
-    await asyncio.sleep(3)
-    print(f"Sending move: {move}")
-    await send_move(move)
+  moves = ["e4", "e5", "Rs4", "Nc6", "Bb5", "a6"]
+  for board_id in boards:
+    for move in moves:
+      await asyncio.sleep(3)
+      print(f"Sending move: {move} to board {board_id}")
+      await send_move(board_id, move)
     
-  await asyncio.sleep(5)
-  print("Resetting game...")
-  await reset_game()
-  moves = ["a3", "g6", "c4", "Bg7", "d4", "Nf6"]
+  # await asyncio.sleep(5)
+  # print("Resetting game...")
+  # await reset_game(board_id)
+  # moves = ["a3", "g6", "c4", "Bg7", "d4", "Nf6"]
   
-  for move in moves:
-    await asyncio.sleep(3)
-    print(f"Sending move: {move}")
-    await send_move(move)
+  # for board_id in boards:
+  #   for move in moves:
+  #     await asyncio.sleep(3)
+  #     print(f"Sending move: {move} to board {board_id}")
+  #     await send_move(board_id, move)
     
 @app.on_event("startup")
 async def start_fake_moves() -> None:
