465fb5e62019b3644defbe321120cfd90f92c972
birgilv <birgittevik84@gmail.com>
2025-03-31 14:13:17 +0200

    Created a tableview to display components

---
+++ frontend/src/pages/tableview.tsx
@@ -0,0 +1,32 @@
+import Chessboard from '../components/chessboard/chessboard';
+import PGN from '../components/pgn/pgn';
+import './TableView.css';
+
+/**
+ * TableView Component
+ *
+ * This layout component arranges the chessboard and PGN move list side by side.
+ * It passes move-related props to the corresponding child components:
+ * - `Chessboard` handles the game state and piece interaction
+ * - `PGN` displays the move history in a tabular format
+ */
+
+interface TableViewProps {
+  moves: string[]; // Array of move strings (algebraic notation)
+  setMoves: React.Dispatch<React.SetStateAction<string[]>>; // Function to update the list of moves
+}
+
+function TableView({ moves, setMoves }: TableViewProps) {
+  return (
+    <div className="table-view">
+      <div className="chessboard-wrapper">
+        <Chessboard setMoves={setMoves} />
+      </div>
+      <div className="pgn-wrapper">
+        <PGN moves={moves} />
+      </div>
+    </div>
+  );
+}
+
+export default TableView;
