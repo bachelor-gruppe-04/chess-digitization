a89e3181d548c62551e90d8f823ccea2e6a6b38c
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2025-03-31 11:35:28 +0200

    Removed setter-methods

--- backend/logic/api/camera.py
+++ backend/logic/api/camera.py
@@ -4,32 +4,16 @@ from typing import Generator
 class Camera:
   """ Camera class to handle webcam. """
   
-  def __init__(self, cam_id: int) -> None:
+  def __init__(self, id: int) -> None:
     """ Initialize the camera object.
 
     Args:
       cam_id (int): Camera ID
     """
-    self.set_cam_id(cam_id)
-    self.camera = cv2.VideoCapture(self.cam_id)
-    
-    
-    
-  def set_cam_id(self, cam_id: int) -> TypeError | None:
-    if not isinstance(cam_id, int):
-      raise TypeError(f"cam_id must be an integar, got {type(cam_id).__name__}")
-    if cam_id < 0:
-      raise ValueError(f"cam_id must be a positive number, got {cam_id}")
-    
-    self.cam_id = cam_id
-    
-    
-    
-  def get_cam_id(self) -> int:
-    """ Get the camera ID. """
-    return self.cam_id
+    self.cam_id = id
+    self.camera = cv2.VideoCapture(id)
   
-  
+
 
   def generate_frames(self) -> Generator[bytes, None, None]:
     """ Generate frames from the laptop webcam.
