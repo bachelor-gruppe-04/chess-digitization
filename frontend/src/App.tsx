e879dded1744d3ea21c8eda6f24936d6de9cc705
birgilv <birgittevik84@gmail.com>
2025-04-03 12:33:36 +0200

    Updated comments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,6 @@ import TournamentView from './pages/tournamentview/tournamentview';
  * App Component
  *
  * This is the main entry point of the React application.
- * It maintains the state of the chess moves and passes it
- * down to the `TableView` component.
  */
 
 function App() {
