8b0114f3b63c9ba1fd824d2a626c06e8f1bf641b
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2025-04-29 13:45:24 +0200

    Made into a class

--- backend/logic/api/services/board_service.py
+++ backend/logic/api/services/board_service.py
@@ -1,20 +1,18 @@
 import asyncio
-from logic.api.entity.board import Board
-from logic.api.config import BOARD_COUNT
+# from logic.api.entity.board import Board
+# from logic.api.config import BOARD_COUNT
+from logic.api.services.board_storage import boards
 
 # boards = {i: Board(i) for i in range(1, (BOARD_COUNT + 1))}
 
 class BoardService:
-  
-  def __init__(self, boards:dict[int, Board]):
-    self.boards = boards
 
   async def start_detectors(self) -> None:
-    for board_id in self.boards:
+    for board_id in boards:
       asyncio.create_task(self.start_detector(board_id))
       
   async def start_detector(self, id:int) -> None:
-    asyncio.create_task(self.boards[id].camera.detector.run())
+    asyncio.create_task(boards[id].camera.detector.run())
 
   async def send_move(self, board_id: int, move: str):
     """ Send a chess move to all clients.
@@ -23,21 +21,22 @@ class BoardService:
       board_id (int): Board ID
       move (str): Chess move
     """
-    board = self.boards[board_id]
-    print(board)
+    board = boards[board_id]
+    print(board.clients)
     checked_move, valid = board.validate_move(move)
-    print(checked_move, valid)
     if valid:
       for client in board.clients:
         await client.send_text(checked_move)
+        print(f"Move {move} sent to board {board_id}")
 
   async def reset_game(self, board_id: int):
     """ Reset the chess game of a board. """
-    board = self.boards[board_id]
+    board = boards[board_id]
     for client in board.clients:
       await client.send_text(board.reset_board())
+      print(f"reset_games() has been called")
 
   async def reset_all_games(self):
     """ Reset the chess game to all boards. """
-    for board_id in self.boards:
+    for board_id in boards:
       await self.reset_game(board_id)
