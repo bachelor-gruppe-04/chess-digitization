d480631fcb76817dd9197819b930fd7353acc1fe
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2025-05-02 17:07:14 +0200

    Removed comments

--- backend/logic/api/entity/detector.py
+++ backend/logic/api/entity/detector.py
@@ -2,9 +2,8 @@ from logic.machine_learning.run_video import prepare_to_run_video
 import cv2
 
 class Detector:
-  def __init__(self, id: int):#, video: cv2.VideoCapture):
+  def __init__(self, id: int):
     """Class to handle video processing for chessboard detection."""
-    # self.video = video
     self.id = id
     
   async def run(self):
