c8b91f2ac19021f9990eda71ceb9ff8c76cb51ce
Vegard Arnesen Mytting <120718174+VegardMytting@users.noreply.github.com>
2025-05-05 10:27:30 +0200

    Created set methods for class obkects

--- backend/logic/api/entity/detector.py
+++ backend/logic/api/entity/detector.py
@@ -4,6 +4,22 @@ import cv2
 class Detector:
   def __init__(self, id: int):
     """Class to handle video processing for chessboard detection."""
+    self.set_id(id)
+    
+  def set_id(self, id: int) -> None:
+    """ Set the ID of the detector. 
+    
+    Args:
+      id (int): Detector ID
+    Raises:
+      TypeError: If the ID is not an integer.
+      ValueError: If the ID is a negative integer.
+    """
+    if not isinstance(id, int):
+      raise TypeError("ID must be an integer.")
+    if id < 1:
+      raise ValueError("ID must be a positive integer.")
+    
     self.id = id
     
   async def run(self):
